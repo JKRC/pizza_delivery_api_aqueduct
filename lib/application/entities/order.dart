c935167d032fae6d0ff95810fa98bd214d686af2
John Kevid <johnkevid@hotmail.com>
2020-09-30 17:14:04 -0300

    feat: orders

---
+++ lib/application/entities/order.dart
@@ -0,0 +1,24 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'package:pizza_delivery_api/application/entities/order_items.dart';
+import 'package:pizza_delivery_api/application/entities/user.dart';
+
+part 'order.g.dart';
+
+@JsonSerializable()
+class Order {
+  final int id;
+  final String paymentType;
+  final String address;
+  final List<OrderItems> items;
+
+  Order({
+    this.id,
+    this.paymentType,
+    this.address,
+    this.items,
+  });
+
+  factory Order.fromJson(Map<String, dynamic> json) => _$OrderFromJson(json);
+  Map<String, dynamic> toJson() => _$OrderToJson(this);
+}
--- lib/application/routers/routers_configure.dart
+++ lib/application/routers/routers_configure.dart
@@ -1,5 +1,6 @@
 import 'package:pizza_delivery_api/application/routers/i_router_configure.dart';
 import 'package:pizza_delivery_api/modules/menu/menu_routers.dart';
+import 'package:pizza_delivery_api/modules/orders/orders_routers.dart';
 import 'package:pizza_delivery_api/modules/users/users_routers.dart';
 import 'package:pizza_delivery_api/pizza_delivery_api.dart';
 
@@ -8,6 +9,7 @@ class RoutersConfigure {
   final List<IRouterConfigure> routers = [
     UsersRouters(),
     MenuRouters(),
+    OrdersRouters(),
   ];
 
   RoutersConfigure(this._router);
