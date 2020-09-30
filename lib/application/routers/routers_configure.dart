38fa12c8e983f6a3506f98e1149fae75a481d028
John Kevid <johnkevid@hotmail.com>
2020-09-30 03:40:51 -0300

    feat: endpoint menu

--- lib/application/routers/routers_configure.dart
+++ lib/application/routers/routers_configure.dart
@@ -1,10 +1,14 @@
 import 'package:pizza_delivery_api/application/routers/i_router_configure.dart';
+import 'package:pizza_delivery_api/modules/menu/menu_routers.dart';
 import 'package:pizza_delivery_api/modules/users/users_routers.dart';
 import 'package:pizza_delivery_api/pizza_delivery_api.dart';
 
 class RoutersConfigure {
   final Router _router;
-  final List<IRouterConfigure> routers = [UsersRouters()];
+  final List<IRouterConfigure> routers = [
+    UsersRouters(),
+    MenuRouters(),
+  ];
 
   RoutersConfigure(this._router);
 
