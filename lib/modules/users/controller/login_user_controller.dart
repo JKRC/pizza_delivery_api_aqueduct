dd3c4a459c3f6d8f0b0b5116ff352c933c88d792
John Kevid <johnkevid@hotmail.com>
2020-09-29 23:21:53 -0300

    feat: user login

---
+++ lib/modules/users/controller/login_user_controller.dart
@@ -0,0 +1,27 @@
+import 'package:injectable/injectable.dart';
+import 'package:pizza_delivery_api/application/exceptions/user_notfound_exception.dart';
+import 'package:pizza_delivery_api/modules/users/controller/models/login_user_rq.dart';
+import 'package:pizza_delivery_api/modules/users/service/i_user_service.dart';
+import 'package:pizza_delivery_api/pizza_delivery_api.dart';
+
+@Injectable()
+class LoginUserController extends ResourceController {
+  final IUserService _service;
+  LoginUserController(this._service);
+
+  @Operation.post()
+  Future<Response> login(@Bind.body() LoginUserRq requestLogin) async {
+    try {
+      final user =
+          await _service.login(requestLogin.email, requestLogin.password);
+      return Response.ok(
+          {'id': user.id, 'name': user.name, 'email': user.email});
+    } on UserNotfoundException catch (userNotFound) {
+      print(userNotFound);
+      return Response.forbidden();
+    } catch (error) {
+      print(error);
+      return Response.serverError(body: {'message': 'internal server error'});
+    }
+  }
+}
